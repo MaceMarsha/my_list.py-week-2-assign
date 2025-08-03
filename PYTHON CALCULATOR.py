cc73925878ada597ad51d1e857647a685c3c0381
MaceMarsha <ndungumercy550@gmail.com>
2025-08-03 23:09:01 +0300

    Add assignment file

---
+++ PYTHON CALCULATOR.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number (e.g., 30): "))
+num2 = int(input("Enter the second number (e.g., 20): "))
+operation = input("Enter operation (+, -, *, /): ")
+
+print(f"{num1} + {num2} = {num1 + num2}")   # 30 + 20 = 50
+print(f"{num1} - {num2} = {num1 - num2}")   # 30 - 20 = 10
+print(f"{num1} * {num2} = {num1 * num2}")   # 30 * 20 = 600
+print(f"{num1} / {num2} = {num1 / num2}")   # 30 / 20 = 1.5
